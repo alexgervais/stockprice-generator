5bebe23acb4ccb598be061945317025e276dde7e
alexgervais <alex.gervais@gmail.com>
2015-05-05 19:18:26 -0400

    generator with variable datapoints & downloadable csv file

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,35 @@ var json2csv = require('json2csv');
 var fs = require('fs');
 var router = express.Router();
 
-router.get('/', function (req, res) {
+var generateDataPoints = function (count, initialTimestamp) {
 
-    var limit = 100000;
-    var timestamp = moment('2011-11-07');
     var y = 0;
-    var data = [];
-    var dataSeries = {type: 'line'};
     var dataPoints = [];
-    for (var i = 0; i < limit; i += 1) {
-        var atTime = timestamp.add(15, 'seconds');
+
+    for (var i = 0; i < count; i += 1) {
+        var atTime = initialTimestamp.add(15, 'seconds');
 
         y += Math.random() * 10 - 5;
+        var parsedValue = parseFloat(Math.abs(y).toFixed(3));
         dataPoints.push({
             x: atTime.unix(),
-            y: Math.abs(y),
+            y: parsedValue,
             time: atTime.format(),
-            value: Math.abs(y)
+            value: parsedValue
         });
     }
+
+    return dataPoints;
+};
+
+router.get('/', function (req, res) {
+
+    var limit = req.query.points || 10;
+    var timestamp = moment('2015-01-01');
+
+    var data = [];
+    var dataPoints = generateDataPoints(limit, timestamp);
+    var dataSeries = {type: 'line'};
     dataSeries.dataPoints = dataPoints;
     data.push(dataSeries);
 
@@ -34,12 +44,12 @@ router.get('/', function (req, res) {
             console.log(err);
         }
         var csvFile = 'stockprices_' + moment().unix() + '.csv';
-        fs.writeFile(csvFile, csv, function (err) {
+        fs.writeFile('public/' + csvFile, csv, function (err) {
             if (err) {
                 console.log(err);
             }
 
-            res.render('index', {title: 'Stockprice Generator', file: csvFile, data: data});
+            res.render('index', {title: 'Stockprice Generator', file: csvFile, points: limit, data: data});
         });
     });
 
